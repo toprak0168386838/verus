bdbeb9da3766b0db71427232e632dfda117e755a
toprak016838 <159837755+toprak016838@users.noreply.github.com>
2024-09-11 23:44:39 +0300

    verus

---
+++ kurverus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+dpkg --configure -a
+apt-get update -y 
+apt-get upgrade -y
+sudo apt-get install unzip -y
+
+
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+
+
+mv /root/verus/verus.sh /root/verus/
+
+cd /root/verus/
+
+screen -dmS verus bash -c 'bash verus.sh'
+
+screen -x verus
+
+
+
+
+
+
+
+
--- verus.sh
+++ verus.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
 
-
-dpkg --configure -a
-apt-get update -y 
-apt-get upgrade -y
-sudo apt-get install unzip -y
-
-
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
-tar -xvf hellminer_linux64.tar.gz
-
-mv /root/sypepe/config.json /root/sypepe/xmrig-6.22.0/
-mv /root/sypepe/sypepe.sh /root/sypepe/xmrig-6.22.0/
-
-cd /root/sypepe/xmrig-6.22.0
-
-screen -dmS sypepe bash -c 'bash sypepe.sh'
-
-screen -x sypepe
-
-
-
-
-
+sudo ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RMv8WEjT75fmsKTspaCbw41uFhEw4bMedH.verus1-p x
 
 
